8799e22efceac8019c09f77a7383ac79b4601db6
NorciaKingmichael <101629927+NorciaKingmichael@users.noreply.github.com>
2023-11-21 07:45:39 +0400

    Update LigSwap.sol

--- LigSwap.sol
+++ LigSwap.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract TokenSwap {
+    address public owner;
+    IERC20 public tokenA;
+    IERC20 public tokenB;
+
+    event TokensSwapped(address indexed swapper, uint256 amountA, uint256 amountB);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+}
